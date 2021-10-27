085d4307c79bb2780796d4511741e9fba4a2fd6c
Denis Chervisnkiy <chervinskiy_2001@mail.ru>
2021-10-27 23:04:14 +0300

    fix docs auth

---
+++ MentorsBlog.Core.Web/AuthOperationAttribute.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Interfaces;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MentorsBlog.Core.Web
+{
+    public class AuthOperationAttribute : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authAttributes = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>()
+                .Distinct();
+ 
+            if (authAttributes.Any())
+            {
+                if (operation.Parameters == null)
+                    operation.Parameters = new List<OpenApiParameter>();
+ 
+                operation.Parameters.Add(new OpenApiParameter()
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Description = "JWT access token",
+                    Required = true
+                });
+
+                operation.Security = new List<OpenApiSecurityRequirement>();
+ 
+                //Add JWT bearer type
+                operation.Security.Add(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference 
+                            { 
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer" 
+                            }
+                        },
+                        new string[] { }   
+                    }
+                });
+            }
+        }
+    }
+}
--- MentorsBlog/Startup.cs
+++ MentorsBlog/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using MentorsBlog.Core.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -36,10 +37,36 @@ namespace MentorsBlog
                     },
                 });
                 
-                // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
+                
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference 
+                            { 
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer" 
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+                
+                c.OperationFilter<AuthOperationAttribute>();
             });
         }
 
